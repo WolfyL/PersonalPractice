bc256241a6fff19d0de090a0522ec6e3f3a48b45
Wolfy <s.leheup@gmail.com>
2017-04-11 19:01:37 +0200

    Front done

--- app/js/controllers/mainController.js
+++ app/js/controllers/mainController.js
@@ -1,4 +1,32 @@
 angular.module('app')
     .controller('MainController', function($scope) {
-      /* Here is your main controller */
+        $scope.tab = [];
+        var maLine;
+
+        $scope.add = function() {
+            maLine = {
+                group: $scope.group,
+                title: $scope.title,
+            };
+            $scope.tab.push(maLine);
+            $scope.group = '';
+            $scope.title = '';
+        };
+
+        $scope.editLine = function() {
+            $scope.editMe = true;
+        };
+
+        $scope.editDone = function(index, newGroup, newTitle) {
+            line = {
+                group: newGroup,
+                title: newTitle
+            };
+            $scope.tab.splice(index, 1, line);
+            $scope.editMe = false;
+        };
+
+        $scope.deleteLine = function(index) {
+            $scope.tab.splice(index, 1);
+        };
     });
