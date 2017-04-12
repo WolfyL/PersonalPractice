17e90d4ebd4d2dd07df0cd9ee9019a7d2ac606a4
Wolfy <s.leheup@gmail.com>
2017-04-12 19:19:05 +0200

    correction warning model music

--- server/api/models/music.js
+++ server/api/models/music.js
@@ -56,7 +56,7 @@ export default class Music {
     }
 
     update(req, res) {
-      console.log("updating:", req.params.id, "[ body:", req.body)
+      console.log("updating:", req.params.id, "[ body:", req.body);
         model.findByIdAndUpdate({
             _id: req.params.id
         }, req.body, (err, music) => {
